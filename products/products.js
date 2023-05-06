8bcb1a72c3b0370fe8cce7f3ee394070583de718
FadeCap <stian94lb@gmail.com>
2023-05-06 16:32:47 +0200

    Finished loader and cleaned up code

--- products/products.js
+++ products/products.js
@@ -1,6 +1,6 @@
 const id = window.location.search.replace("?", "");
 const root = document.getElementById("root");
-const url = `https://api.noroff.dev/api/v1/rainy-days/${id}`;
+const url = `https://api.noroff.dev/api/v1/rainy-dayss/${id}`;
 
 function printSizes(arr) {
   let sizes = "";
@@ -11,34 +11,56 @@ function printSizes(arr) {
 }
 
 async function getProducts() {
-  const response = await fetch(url);
+  try {
+    root.innerHTML = `
+      <div class="loader">
+        <div class="spinner"></div>
+      </div>
+    `;
 
-  const results = await response.json();
+    const response = await fetch(url);
 
-  const products = results;
+    const results = await response.json();
 
-  root.innerHTML += `<div class="container" id=${products.id}>
-                        <img
-                            class="jacket"
-                            src=${products.image}
-                            alt=${products.title}
-                        />
-                        <div class="product-info-container">
-                            <h2 class="product-name">${products.title}</h2>
-                            <div class="size">
-                            ${printSizes(products.sizes)}
-                            </div>
-                            <h3 class="price">
-                            ${
-                              products.onSale
-                                ? products.discountedPrice
-                                : products.price
-                            }
-                            </h3>
-                            <p class="product-description">${products.description}</p>
-                            <button class="btn">Add to cart</button>
-                        </div>
-                    </div>`;
+    const products = results;
+
+    root.innerHTML = `
+      <div class="container" id=${products.id}>
+        <img
+            class="jacket"
+            src=${products.image}
+            alt=${products.title}
+        />
+        <div class="product-info-container">
+            <h2 class="product-name">
+              ${products.title}
+            </h2>
+            <div class="size">
+              ${printSizes(products.sizes)}
+            </div>
+            <h3 class="price">
+              ${
+                products.onSale
+                  ? products.discountedPrice
+                  : products.price
+              }
+            </h3>
+            <p class="product-description">
+              ${products.description}
+            </p>
+            <button class="btn">Add to cart</button>
+        </div>
+      </div>
+    `;
+  } catch (error) {
+    console.error("There has been an", error);
+    root.innerHTML = `
+      <div class="error-container"> 
+        <h1 id="error">We're experiencing technical difficulties</h1>
+        <a class="btn" href="/collection">Go back</a> 
+      </div>
+    `;
+  }
 }
 
 getProducts();
